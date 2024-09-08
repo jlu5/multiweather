ba656448ca3ec0952d53c7be2f660d72fa156ca4
James Lu <james@overdrivenetworks.com>
2024-09-07 20:10:08 -0700

    Use a more informative UA

--- multiweather/backends/basebackend.py
+++ multiweather/backends/basebackend.py
@@ -10,12 +10,17 @@ from multiweather.data import (
 )
 
 from multiweather.log import logger
+from multiweather.version import __version__
 
 type LatLon = tuple[float, float]
 # type Location = LatLon | str
 
 class APIError(Exception):
     """Generic class for API errors"""
+
+DEFAULT_HEADERS = {
+    'User-Agent': f'Mozilla/5.0 (compatible; python-multiweather/{__version__})'
+}
 
 class BaseWeatherBackend(ABC):
     """Base class for weather backends"""
@@ -36,17 +41,19 @@ class BaseJSONWeatherBackend(BaseWeatherBackend):
     def _format_weather(self, location, data) -> WeatherResponse:
         """Given a weather API's JSON response, generate a WeatherResponse instance"""
 
-    async def get_weather(self, location) -> WeatherResponse:
+    async def get_weather(self, location, headers=None) -> WeatherResponse:
         request_url = self._get_json_request_url(location)
+        headers = headers or DEFAULT_HEADERS
         logger.debug("Using URL %s", request_url)
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(headers=headers) as session:
             async with session.get(request_url) as resp:
                 json_data = await resp.json()
                 return self._format_weather(location, json_data)
 
-    def get_weather_sync(self, location, timeout=10) -> WeatherResponse:
+    def get_weather_sync(self, location, timeout=10, headers=None) -> WeatherResponse:
         request_url = self._get_json_request_url(location)
+        headers = headers or DEFAULT_HEADERS
         logger.debug("Using URL %s", request_url)
-        resp = requests.get(request_url, timeout=timeout)
+        resp = requests.get(request_url, timeout=timeout, headers=headers)
         json_data = resp.json()
         return self._format_weather(location, json_data)
