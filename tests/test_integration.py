2913a8eeb67585c9b4f091ff0447dd2e9a8efcda
James Lu <james@overdrivenetworks.com>
2024-09-08 22:11:41 -0700

    Add OpenWeatherMap integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,9 +6,11 @@ import unittest
 
 from multiweather import (
     OpenMeteoBackend,
+    OpenWeatherMapBackend,
     PirateWeatherBackend,
 )
 
+API_KEY_OPENWEATHERMAP = os.environ.get('API_KEY_OPENWEATHERMAP')
 API_KEY_PIRATEWEATHER = os.environ.get('API_KEY_PIRATEWEATHER')
 
 class BaseTestCase:
@@ -16,8 +18,10 @@ class BaseTestCase:
     # https://stackoverflow.com/a/25695512
     class IntegrationTestBase(unittest.IsolatedAsyncioTestCase):
         MAX_TIME_DRIFT = datetime.timedelta(hours=12)
+        TEST_FORECAST_DAYS = 3
         backend = None
         supports_geocoding = False
+        supports_daily_forecast = False
 
         def _smoke_test_weather(self, weatherdata):
             """Run basic checks on the weather data (independent of the actual reported values)"""
@@ -31,7 +35,8 @@ class BaseTestCase:
             self.assertIsNotNone(weatherdata.current.temperature)
 
             # Daily forecasts
-            self.assertTrue(weatherdata.daily_forecast)
+            if self.supports_daily_forecast:
+                self.assertGreaterEqual(len(weatherdata.daily_forecast), self.TEST_FORECAST_DAYS)
             last_datetime = None
             # Forecast times should be monotonically increasing
             for daily_forecast in weatherdata.daily_forecast:
@@ -49,7 +54,8 @@ class BaseTestCase:
 
         def test_integration_sync(self):
             """Test the synchronous get_weather path"""
-            w = self.backend.get_weather_sync((49.000000, -123.000000))
+            forecast_days = self.TEST_FORECAST_DAYS if self.supports_daily_forecast else 0
+            w = self.backend.get_weather_sync((49.000000, -123.000000), forecast_days=forecast_days)
             self._smoke_test_weather(w)
 
         async def test_integration_async(self):
@@ -71,16 +77,22 @@ class BaseTestCase:
             w = self.backend.get_weather_sync('Vancouver')
             self._smoke_test_weather(w)
 
-@unittest.skipUnless(API_KEY_PIRATEWEATHER, "API_KEY_PIRATEWEATHER env var not set")
-class TestPirateWeather(BaseTestCase.IntegrationTestBase):
-    def setUp(self):
-        self.backend = PirateWeatherBackend(API_KEY_PIRATEWEATHER)
-
 class TestOpenMeteo(BaseTestCase.IntegrationTestBase):
     def setUp(self):
         # Disable fill_current_with_hourly to save a few API calls
         self.backend = OpenMeteoBackend(fill_current_with_hourly=False)
         self.supports_geocoding = True
+
+@unittest.skipUnless(API_KEY_OPENWEATHERMAP, "API_KEY_OPENWEATHERMAP env var not set")
+class TestOpenWeatherMap(BaseTestCase.IntegrationTestBase):
+    def setUp(self):
+        self.backend = OpenWeatherMapBackend(API_KEY_OPENWEATHERMAP)
+        self.supports_geocoding = True
+
+@unittest.skipUnless(API_KEY_PIRATEWEATHER, "API_KEY_PIRATEWEATHER env var not set")
+class TestPirateWeather(BaseTestCase.IntegrationTestBase):
+    def setUp(self):
+        self.backend = PirateWeatherBackend(API_KEY_PIRATEWEATHER)
 
 if __name__ == '__main__':
     unittest.main()
